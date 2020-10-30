21a7ad449a850c5f1bfd111b88520d5d81053ae8
Jonah-Checketts <73562969+Jonah-Checketts@users.noreply.github.com>
2020-10-30 06:48:13 -0400

    added a basic board
    I added a basic board to my python canvas but I still need to make it more user friendly to be able to put in a number into without a tone of formatting.

--- SudokuAlgorithm.py
+++ SudokuAlgorithm.py
@@ -1,1 +1,27 @@
-import tkinter as tk
+#import tkinter to your file
+from tkinter import *
+
+#main window
+root = Tk()
+
+#creating the canvas
+Canvas = Canvas(root, width = 500, height = 500)
+
+#main vertical lines for the board
+Canvas.create_line(2,0,2,500)
+Canvas.create_line(166,0,166,500)
+Canvas.create_line(333,0,333,500)
+Canvas.create_line(500,0,500,500)
+
+
+#horizontal lines for the board
+Canvas.create_line(0,2,500,2)
+Canvas.create_line(0,166,500,166)
+Canvas.create_line(0,333,500,333)
+Canvas.create_line(0,500,500,500)
+
+#putting the canvas on the main window
+Canvas.pack()
+
+#main loop
+root.mainloop()
