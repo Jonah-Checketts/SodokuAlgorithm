4de68b6b71cf16c6c7709c614869efc512987ec5
Jonah-Checketts <73562969+Jonah-Checketts@users.noreply.github.com>
2020-11-03 13:08:30 -0500

    Finished the project
    I finished the project with the logic and updated the GUI to make it so it is much easier to manage than the lines I drew on a canvas in my last iteration. The GUI still isn't super good looking and has some bugs. The algorithm mixed with the GUI also leads to very slow speeds and the program eventually crashing so I am going to include the same code but without a GUI to show that it does work.

--- SudokuAlgorithm.py
+++ SudokuAlgorithm.py
@@ -1,27 +1,85 @@
-#import tkinter to your file
 from tkinter import *
+from time import *
 
 #main window
 root = Tk()
+root.minsize(350,305)
+root.maxsize(350,305)
+board = [[8,0,0,0,0,0,0,0,0],
+         [0,0,3,6,0,0,0,0,0],
+         [0,7,0,0,9,0,2,0,0],
+         [0,5,0,0,0,7,0,0,0],
+         [0,0,0,0,4,5,7,0,0],
+         [0,0,0,1,0,0,0,3,0],
+         [0,0,1,0,0,0,0,6,8],
+         [0,0,8,5,0,0,0,1,0],
+         [0,9,0,0,0,0,4,0,0]]
+objectBoard =[[0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0]]
 
-#creating the canvas
-Canvas = Canvas(root, width = 500, height = 500)
+class square():
+    def __init__(self,x,y,t,root,color):
+        self.tbox = Label(root,text = str(t), height=2, width=5,borderwidth = 1, relief = "solid", bg = color)
+        self.tbox.grid(row = y, column = x)
+    def changeColor(self,color):
+        self.tbox.config(bg = color)
+    def changeNum(self,t):
+        self.tbox.configure(text = str(t))
 
-#main vertical lines for the board
-Canvas.create_line(2,0,2,500)
-Canvas.create_line(166,0,166,500)
-Canvas.create_line(333,0,333,500)
-Canvas.create_line(500,0,500,500)
+def buildBoard(board):
+    for y in range(9):
+        for x in range(9):
+            if objectBoard[y][x] != 0:
+                objectBoard[y][x] = square(x,y,(board[y][x]),root, objectBoard[y][x].tbox.cget("bg"))
+            else:
+                objectBoard[y][x] = square(x,y,(board[y][x]),root,"#ffffff")
 
 
-#horizontal lines for the board
-Canvas.create_line(0,2,500,2)
-Canvas.create_line(0,166,500,166)
-Canvas.create_line(0,333,500,333)
-Canvas.create_line(0,500,500,500)
+def isPossible(x, y, n):
+    cx = (x//3)*3
+    cy = (y//3)*3
+    for yl in range(9):
+        if board[yl][x] == n:
+            return False
+    for xl in range(9):
+        if board[y][xl] == n:
+            return False
+    for yl in range(3):
+        for xl in range(3):
+            if board[yl+cy][xl+cx] == n:
+                return False
+    return True
+def solve():
+    global objectBoard
+    global board
+    for y in range(9) :
+        for x in range(9) :
+            if board[y][x] == 0 :
+                objectBoard[y][x].changeColor("blue")
+                buildBoard(board)
+                root.update()
+                for n in range(1,10):
+                    if isPossible(x,y,n):
+                        board[y][x] = n
+                        print("("+ str(x) + ","+str(y)+"): " + str(n))
+                        objectBoard[y][x].changeColor("green")
+                        buildBoard(board)
+                        root.update()
+                        solve()
+                        board[y][x] = 0
+                        objectBoard[y][x].changeColor("#ffffff")
+                        buildBoard(board)
+                        root.update()
+                return
+    print(board)
 
-#putting the canvas on the main window
-Canvas.pack()
-
-#main loop
+buildBoard(board)
+solve()
 root.mainloop()
